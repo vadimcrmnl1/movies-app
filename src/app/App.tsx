94038897b68812e7fed8b897180f8f52b1aecd76
Vadzim <vadzimkarpenka@gmail.com>
2024-05-14 17:41:32 +0300

    fix Routes.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,23 +8,8 @@ import s from '../app/App.module.css'
 import {Navigate, Outlet} from "react-router-dom";
 
 export const App = () => {
-
-    // const dispatch = useAppDispatch()
     const isLoading = useSelector(selectIsLoading)
-    // const requestToken = useAppSelector(selectRequestToken)
-    // const page = useAppSelector(selectPage)
-    // const genre = useAppSelector(selectGenre)
-    // const year = useAppSelector(selectPrimaryReleaseYear)
-    // const averageGte = useAppSelector(selectAverageGte)
-    // const averageLte = useAppSelector(selectAverageLte)
-    // const sortBy = useAppSelector(selectSortBy)
-    //
-    //
-    // useEffect(() => {
-    //     // dispatch(createGuestSession())
-    //     dispatch(fetchMovies())
-    //     dispatch(fetchGenres())
-    // }, [page, genre, year, averageGte, averageLte, sortBy])
+
     Navigate({to: '/movies-app/movies'})
     return <MantineProvider theme={theme}>
         {isLoading && <LoaderComponent/>}
