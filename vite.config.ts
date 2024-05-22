06c0b97176c9a8f48863e98f95132900f1bb21f3
Vadzim <vadzimkarpenka@gmail.com>
2024-05-22 23:41:34 +0300

    fix Routes.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     base: '/movies-app/',
-    root: path.resolve(__dirname, 'src')
 })
