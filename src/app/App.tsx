b14700eed1a245ed8c33ab6b68d16e8f69e76796
Vadzim <vadzimkarpenka@gmail.com>
2024-05-26 14:00:30 +0300

    without Routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,17 +5,17 @@ import {useSelector} from "react-redux";
 import {selectIsLoading} from "./selectors.ts";
 import {LoaderComponent} from "../common/components/Loader/Loader.tsx";
 import s from '../app/App.module.css'
+import {Navigate, Outlet} from "react-router-dom";
 
 export const App = () => {
     const isLoading = useSelector(selectIsLoading)
 
-    // Navigate({to: '/movies-app/movies'})
+    Navigate({to: '/movies-app/movies'})
     return <MantineProvider theme={theme}>
         {isLoading && <LoaderComponent/>}
         <div className={s.wrapper}>
             <div className={s.container}>
-                {/*<Outlet/>*/}
-                Hello world
+                <Outlet/>
             </div>
         </div>
     </MantineProvider>;
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import ReactDOM from "react-dom/client";
 import {Provider} from "react-redux";
 import {store} from "./app/store";
 import React from "react";
-import {App} from "./app/App.tsx";
+import {RouterProvider} from "react-router-dom";
+import router from "./common/routes/Routes.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <Provider store={store}>
-            {/*<RouterProvider router={router}/>*/}
-            <App/>
+            <RouterProvider router={router}/>
         </Provider>
     </React.StrictMode>
 );
